1c8732a4223341385efabf2a356fc4a02e4c77d8
Carlos Mañas <carlos@spacenomads.com>
2022-12-18 09:14:03 +0100

    Add script

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# PassiveAgressiveMe
-Alfred Workflow to add a passive-aggressive tone to any text as seen in this reel https://www.instagram.com/reel/CjBBjejAg1r/?=YmMyMTA2M2Y=
+# paSSiveAGgrESsIVEMe
+
+aLfred worKfLoW to ADD A pasSive-AGGRessIVe tonE TO anY teXT As seEn In [thIS insTagRam ReeL](https://www.instagram.com/reel/CjBBjejAg1r/?=YmMyMTA2M2Y=)
+
+
---
+++ _src/script.js
@@ -0,0 +1,21 @@
+const TEXT = 'Alfred Workflow to add a passive-aggressive tone to any text as seen in this Instagram reel';
+
+function getRandomNumber(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function randomCase(char) {
+	const format = [0, 0, 1];
+	const MIN = 0;
+	const random = getRandomNumber(MIN, format.length);
+	return format[random] === 0 ? char.toLowerCase() : char.toUpperCase();
+}
+
+function passiveAggressiveMe(str) {
+	return str
+		.split('')
+		.map(randomCase)
+		.join('');
+}
+
+console.log(passiveAggressiveMe(TEXT));
